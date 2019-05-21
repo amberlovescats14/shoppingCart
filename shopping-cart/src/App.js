a4ce8ae5e66193e29a897a06b5c5ef2994973742
Amber Jones <amberlovescats14@gmail.com>
2019-05-21 10:08:12 -0500

    react-bootstrap

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import { Grid } from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-    <h1>APP</h1>
+    <div className="container">
+    <Grid className="purchase-card">
+    <h1>hellow world</h1>
+    </Grid>
     </div>
   );
 }
